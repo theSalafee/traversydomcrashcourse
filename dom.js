7e154b5c842d52f8062aadce635f73b049975763
Naasir Salafee <naasirbush@icloud.com>
2022-12-17 07:54:55 -0800

    New branch for the actual work

--- dom.js
+++ dom.js
@@ -1,1 +1,20 @@
 // Examine the Document Object Model (DOM) of a web page
+
+/*
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+// document.title = "New title";
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[10]);
+console.log(document.forms);
+console.log(document.links);
+*/
+
+// GetElementById
+console.log(document.getElementById("header-title"));
+
+

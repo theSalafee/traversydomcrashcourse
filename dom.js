a5af861f00ddea3cca2603fe9b5edbe75a964208
Naasir Salafee <naasirbush@icloud.com>
2022-12-17 07:42:14 -0800

    Added the doctype

--- dom.js
+++ dom.js
@@ -1,1 +1,6 @@
-console.dir(document);
+// Examine the Document Object Model (DOM) of a web page
+
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.doctype);
